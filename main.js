2886515bc9f42db5471c0927c8e8a7a7ed894a9a
shaili-dixit <dixitshaili5@gmail.com>
2025-07-31 12:27:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -74,3 +74,23 @@ const swiper = new Swiper(".swiper", {
   spaceBetween: 20,
   loop: true,
 });
+
+
+
+const toggleSwitch = document.getElementById("darkModeSwitch");
+  const currentTheme = localStorage.getItem("theme");
+
+  if (currentTheme === "dark") {
+    document.body.classList.add("dark-mode");
+    toggleSwitch.checked = true;
+  }
+
+  toggleSwitch.addEventListener("change", () => {
+    if (toggleSwitch.checked) {
+      document.body.classList.add("dark-mode");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.body.classList.remove("dark-mode");
+      localStorage.setItem("theme", "light");
+    }
+  });
